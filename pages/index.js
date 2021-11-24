a57ec181ec82b6931c2a76d88382116346e1ef68
Maximilian Mitchell <max@max.me.uk>
2021-11-24 18:44:04 +0000

    Revert "add logoUrl"
    This reverts commit 3054b9dacf46feb883d1a874d12d4393eb8383b1.

--- pages/index.js
+++ pages/index.js
@@ -70,10 +70,8 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
       <div className="container mx-auto px-4">
         <div className="flex flex-row justify-between items-center p-4">
           <div className="flex flex-row items-center">
-            <a href={config.settings.logoUrl} target="_blank">
-              <img className="h-8 w-auto" src={config.settings.logo} />
-              <h1 className="ml-4 text-3xl">{config.settings.title}</h1>
-            </a>
+            <img className="h-8 w-auto" src={config.settings.logo} />
+            <h1 className="ml-4 text-3xl">{config.settings.title}</h1>
           </div>
           <div className="flex flex-row items-center">
             {typeof window !== 'undefined' && <ThemeSwitcher />}
